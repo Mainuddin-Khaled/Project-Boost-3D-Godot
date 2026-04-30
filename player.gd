fbb4797d620af06f7fc65eee32cd626f2b7074a0
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-05-01 01:57:51 +0600

    First Commit
    Project setup, code, and physics.

---
+++ player.gd
@@ -0,0 +1,10 @@
+extends RigidBody3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		apply_central_force(basis.y * delta * 1000.0)
+	if Input.is_action_pressed("ui_left"):
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+	if Input.is_action_pressed("ui_right"):
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
