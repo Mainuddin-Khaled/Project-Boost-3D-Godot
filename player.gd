c2fab166c219519382c219f26062f5699d4028a2
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-05-30 20:12:58 +0600

    Fourth Commit

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
+var is_transitioning: bool = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Boost"):
@@ -13,15 +14,26 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if 'Goal' in body.get_groups():
-		complete_level()
-	if 'Hazard' in body.get_groups():
-		crash_sequence()
+	if is_transitioning == false:
+		if 'Goal' in body.get_groups():
+			complete_level(body.file_path)
+		if 'Hazard' in body.get_groups():
+			crash_sequence()
 
 func crash_sequence() -> void:
 	print('You Lose!')
-	get_tree().reload_current_scene()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 
-func complete_level() -> void:
+func complete_level(next_level_file: String) -> void:
 	print('You Win!')
-	get_tree().quit()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(
+		get_tree().change_scene_to_file.bind(next_level_file)
+	)
